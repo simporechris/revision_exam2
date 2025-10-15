f6ee93e7bb8243f7ec6518d9eafc761866ce5118
simporechris <2468242@cegepoutaouais.qc.ca>
2025-10-14 23:00:51 -0400

    k

--- débutant.py
+++ débutant.py
@@ -1,29 +1,8 @@
-def  arme (ls_Galadriel,ls_Sauron):
-    ls_Galadriel = [250,150,500]
-    ls_Sauron = [100,200,2500]
+def  arme ():
+    ls_arme = [
+    250,150,500,
+    100,200,2500
+]
 
 def strategie (ls_strategie):
     ls_strategie = [-500,+175]
-
-
-"""elfes=trolls
-    nains=worgs
-    humains=orcs
-    humains= elfes>=10 or nains>=5  
-
-
-
-dict_:{
-    "elfes" : 250,
-    "nains" : 150,
-    "humains" : 500,
-}
-
-dict_Sauron:{
-    "trolls":100,
-    "worgs": 200,
-    "orcs": 2500
-}
-def strategie (agressif, defensif):
-    agressif= Sauron("","",-500)
-    defensif = Galadriel("",+175,"")"""
